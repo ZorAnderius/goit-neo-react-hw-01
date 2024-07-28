8710071f941f63f9b94f5ba2342e2a9f1b0065e3
ZorAnderius <gabriel.zorander@gmail.com>
2024-07-28 09:14:48 +0100

    fixe styles css for transictions

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // build: {
-  //   sourcemap: true,
-  // },
-  // base: '/goit-neo-react-hw-01/',
+  base: '/goit-neo-react-hw-01/',
 })
